d02688e6d864e6a56de1ffaecde6ad2214458349
Noe Tellez <noetlz10@gmail.com>
2017-07-22 19:08:32 -0500

    added friends array and dependencies

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// Data structure that will store the friends data
+var friendsArray = [{
+        name: 'Mario',
+        photo: 'http://balitangviral.com/wp-content/uploads/2015/09/Super-Mario-08.png',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Luigi',
+        photo: 'http://i2.kym-cdn.com/photos/images/original/000/783/975/c4e.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Bowser',
+        photo: 'http://vignette2.wikia.nocookie.net/mario/images/6/6c/Bowser%2C_Super_Mario_64_DS.png/revision/latest?cb=20121106012958',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Peach',
+        photo: 'http://vignette2.wikia.nocookie.net/nintendo/images/d/de/Princess_Peach_%28Fortune_Street%29.png/revision/latest?cb=20130625131449&path-prefix=en',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Yoshi',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/3/39/YoshiMarioParty9.png',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Toad',
+        photo: 'https://www.mariowiki.com/images/thumb/d/db/SMR_Toad.png/170px-SMR_Toad.png',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Koopa',
+        photo: 'https://www.mariowiki.com/images/4/48/MP10KoopaTroopa.png',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Donkey Kong',
+        photo: 'https://www.mariowiki.com/images/thumb/c/cb/Donkey_Kong_Artwork_%28alt%29_-_Donkey_Kong_Country_Tropical_Freeze.png/300px-Donkey_Kong_Artwork_%28alt%29_-_Donkey_Kong_Country_Tropical_Freeze.png',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Wario',
+        photo: 'https://www.mariowiki.com/images/thumb/2/22/Wario_MP10.png/250px-Wario_MP10.png',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Boo',
+        photo: 'https://www.mariowiki.com/images/thumb/a/a1/Boo_CTTT.png/200px-Boo_CTTT.png',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
